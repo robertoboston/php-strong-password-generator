e832e794e5ca6fae3b5dca46e5207a8c9c258a28
roberto.boston <roberto.bozzo97@tiscali.it>
2023-01-31 14:45:31 +0100

    form created

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel= "stylesheet" href="./css/style.css">
+</head>
+<body>
+    <div class="container">
+        <header>
+            <h1>Strong password generator</h1>
+            <h3>Genera una password sicura</h3>
+        </header>
+
+        <main class="container-main">
+            <form action="./index.php" method="GET">
+                <label for="number">Numero caratteri password</label>
+                <input class="numeri-password" type="number" name="number" id="number" placeholder="numero">
+                <button class="send-button">INVIA</button>
+            </form>
+        </main>
+    </div>
+   
+</body>
+</html>
